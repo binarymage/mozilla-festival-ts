ff5e7c0cbfb00103860bde43f18272a09a1a1571
George Shore <george@georgeshore.com>
2016-08-01 13:31:38 +0100

    Listen for socket events
    Resolves #1
    
    We listen for the `disconnect` event, binding its `this` to the `Game` instance
    and passing the client socket object to the handler. We also have stubs for the
    `new player` and `move player` events.

--- game.ts
+++ game.ts
@@ -13,6 +13,28 @@ class Game {
             transports: ['websocket'],
         });
         this._players = [];
+        this._setEventHandlers.call(this);
+    }
+
+    private _setEventHandlers(): void {
+        this._io.on('connection', this._onClientConnect.bind(this));
+    }
+
+    private _onClientConnect(client: SocketIO.Socket) {
+        util.log(`New player has connected: ${client.id}`);
+        client.on('disconnect', this._onClientDisconnect.bind(this, client))
+            .on('new player', this._onNewPlayer)
+            .on('move player', this._onMovePlayer);
+    }
+
+    private _onClientDisconnect(client: SocketIO.Socket) {
+        util.log(`Player has disconnected: ${client.id}`);
+    }
+
+    private _onNewPlayer() {
+    }
+
+    private _onMovePlayer() {
     }
 }
 
